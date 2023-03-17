738a40975255019ae1b9ae3ac8bfdaec71cae34c
chikanara <usertestabc35@gmail.com>
2023-03-17 16:23:39 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const slide = ["assets/slider-1.jpg", "assets/slider-2.jpg"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
